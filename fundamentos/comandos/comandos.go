7bb87a55761aa95d38a5073aaaf0741323230baf
nathaclmpaulino <nathapaulino17@gmail.com>
2020-12-27 15:00:05 +0000

    [Fundamentos] Capitulo I - Fundamentos.

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,10 @@
+package main 
+
+import "fmt"
+
+func main() {
+	
+	fmt.Printf("Outro programa em %s\n", "Go") 
+
+	// go vet mapeia falhas via linha de comando.
+}
---
+++ fundamentos/helloWorld/hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+/* 
+  Não se pode ter em uma mesma pasta 
+  dois arquivos com a função main
+*/
+
+func main() {
+	fmt.Print("Hello World!")
+}
